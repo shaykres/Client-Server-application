82882f53397ae23e277e6e1f0150b4945c7e2286
noga <nogaschw@post.bgu.ac.il>
2022-01-02 13:37:45 +0200

    yala to run!!

---
+++ spl-net/spl-net/src/main/java/bgu/spl/net/Main/mainReactor.java
@@ -0,0 +1,17 @@
+package bgu.spl.net.Main;
+
+import bgu.spl.net.impl.BidiMessagingProtocolImpl;
+import bgu.spl.net.impl.MessageEncoderDecoderImpl;
+import bgu.spl.net.impl.NetworkSystemData;
+import bgu.spl.net.srv.Server;
+
+import java.io.IOException;
+
+public class mainReactor {
+    public static void main(String[] args) throws IOException {
+        NetworkSystemData data = NetworkSystemData.getInstance();
+        Server.reactor(Integer.parseInt(args[1]), Integer.parseInt(args[0]),
+                () -> new BidiMessagingProtocolImpl(),
+                () -> new MessageEncoderDecoderImpl()).serve();
+    }
+}
