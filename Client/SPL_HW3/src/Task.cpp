71f18da57deb9903c8a2c40a3b027954f0f67e9c
nogaschw <80199057+nogaschw@users.noreply.github.com>
2022-01-05 16:20:03 +0200

    Update Task.cpp

--- Client/SPL_HW3/src/Task.cpp
+++ Client/SPL_HW3/src/Task.cpp
@@ -24,12 +24,12 @@ void Task::run() {
             // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
             answer.resize(len - 1);
             std::cout << "Reply: " << answer  << std::endl << std::endl;
-            if (std::equal(answer.begin(), answer.end(), "ACK 2") ) {
+            if (std::equal(answer.begin(), answer.end(), "ACK 2 ") ) {
                 login = true;
                // std::cout << "user log in" << std::endl;
             }
 
-            if (std::equal(answer.begin(), answer.end(), "ACK 3") ) {
+            if (std::equal(answer.begin(), answer.end(), "ACK 3 ") ) {
                 std::cout << "Exiting...\n" << std::endl;
                 _terminated = true;
             }
