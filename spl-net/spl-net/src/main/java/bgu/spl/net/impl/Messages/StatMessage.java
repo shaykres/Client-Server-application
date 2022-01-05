a81110d3ecc7dcb59299726ebb920f7df664920c
nogaschw <80199057+nogaschw@users.noreply.github.com>
2022-01-05 16:17:49 +0200

    Update StatMessage.java

--- spl-net/spl-net/src/main/java/bgu/spl/net/impl/Messages/StatMessage.java
+++ spl-net/spl-net/src/main/java/bgu/spl/net/impl/Messages/StatMessage.java
@@ -37,34 +37,37 @@ public class StatMessage extends Message {
             noting[3]=opcode[1];
             return noting;
         }
+        int i=0;
+        byte[]opcodeack=shortToBytes((short)10);
+        message[0]=opcodeack[0];
+        message[1]=opcodeack[1];
+        byte[]opcode=shortToBytes(opCode);
+        message[2]=opcode[0];
+        message[3]=opcode[1];
         for (User u: users) {
-            byte[]opcodeack=shortToBytes((short)10);
-            message[0]=opcodeack[0];
-            message[1]=opcodeack[1];
-            byte[]opcode=shortToBytes(opCode);
-            message[2]=opcode[0];
-            message[3]=opcode[1];
             //age
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
             String date = u.getBirthday();
             LocalDate birthday = LocalDate.parse(date, formatter);
             LocalDate now = LocalDate.now();
-            int age= Period.between(birthday, now).getYears();
+            int age=Period.between(birthday, now).getYears();
             byte[]byteage=shortToBytes((short)age);
-            message[4]=byteage[0];
-            message[5]=byteage[1];
+            message[4+9*i]=byteage[0];
+            message[5+9*i]=byteage[1];
             //*****
             byte[]numOfPost=shortToBytes((short)u.getnumPost());
-            message[6]=numOfPost[0];
-            message[7]=numOfPost[1];
+            message[6+9*i]=numOfPost[0];
+            message[7+9*i]=numOfPost[1];
             byte[]numOffollowers=shortToBytes((short)u.getNumOfFollowers());
-            message[8]=numOffollowers[0];
-            message[9]=numOffollowers[1];
+            message[8+9*i]=numOffollowers[0];
+            message[9+9*i]=numOffollowers[1];
             byte[]numOffollowing=shortToBytes((short)u.getNumOffollowing());
-            message[10]=numOffollowing[0];
-            message[11]=numOffollowing[1];
-            message[12]=';';
+            message[10+9*i]=numOffollowing[0];
+            message[11+9*i]=numOffollowing[1];
+            message[12+9*i]='\n';
+            i++;
         }
+        message[12+9*(i-1)]=';';
         return message;
     }
 
